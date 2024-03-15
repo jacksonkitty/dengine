effec1e4ab0fecc8981404f0a0784d3528fcc3c4
Alex Salisbury <alex@alexsalisbury.com>
2024-03-15 14:15:31 -0500

    Config + cleanup

--- src/Satchel/Note.cs
+++ src/Satchel/Note.cs
@@ -4,9 +4,4 @@ public class Note(string filename)
 {
     public string FileName { get; set; } = filename;
     public long Timestamp { get; set; }
-
-    public bool Hydrate()
-    {
-        return false;
-    }
 }
