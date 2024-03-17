d6b762034f6eae60dadb010cbd5d6cdcc2e4c29f
Alex Salisbury <alex@alexsalisbury.com>
2024-03-17 08:08:33 -0500

    Steno refactor: Core, resilient ConfigClient<T>, StenoOptions,

--- src/Quick/QuickSteno/Program.cs
+++ src/Quick/QuickSteno/Program.cs
@@ -1,11 +1,18 @@
-﻿using QuickSteno;
+﻿using Microsoft.Extensions.Configuration;
+using QuickSteno;
 
+var environmentName = "Development";
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile($"appsettings.json", true, true)
+  .AddJsonFile($"appsettings.{environmentName}.json", true, true)
+  .Build();//  .AddEnvironmentVariables();
+
+var options = configuration.GetSection("StenoOptions").Get<StenoOptions>();
 const string defaultRoot = @"C:\notes"; //C:\data\sample\notes
-var httpClient = ConfigClient.MakeClient(new Uri($"http://lithium:8410"));
 
-var example = new ConfigClient(httpClient);
-var key = await example.GetSecretAsync("dev", "steno", "SubscriptionKey");
-var region = await example.GetSecretAsync("dev", "steno", "ServiceRegion");
+var example = new ExampleClient(options);
+var key = await example.GetSecretAsync("SubscriptionKey");
+var region = await example.GetSecretAsync("ServiceRegion");
 
 var spoken = await SpeechService.CaptureSingleUtterance(key?.Value, region?.Value, CancellationToken.None);
 if (spoken is not null) { await FileWriter.WriteResultToFileAsync(defaultRoot, spoken, CancellationToken.None); }
