87a1b45aa25d5781002f757b7252d4e7f54c626f
Alex Salisbury <alex@alexsalisbury.com>
2024-03-17 07:47:48 -0500

    Create a ResilientClientFactory with Options in a (new) Core lib.

---
+++ QuickTools.Core/ResilientClientOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace QuickTools.Core;
+
+public interface IResilientClientOptions
+{
+    string ConfigServiceUrl { get; set; }
+}
+
+public class ResilientClientOptions : IResilientClientOptions
+{
+    public string ConfigServiceUrl { get; set; }
+}
--- src/Quick/QuickConfigClient/Program.cs
+++ src/Quick/QuickConfigClient/Program.cs
@@ -1,34 +1,7 @@
 ﻿using System.Net.Http.Json;
-using Microsoft.Extensions.Http;
-using Polly;
-using Polly.Extensions.Http;
+using QuickTools.Core;
 
-//https://learn.microsoft.com/en-us/dotnet/fundamentals/networking/http/httpclient-guidelines
-#if RESILIENCE_HANDLER_IS_NO_LONGER_EVAL
-var retryPipeline = new ResiliencePipelineBuilder<HttpResponseMessage>()
-    .AddRetry(new HttpRetryStrategyOptions
-    {
-        BackoffType = DelayBackoffType.Exponential,
-        MaxRetryAttempts = 3
-    })
-    .Build();
-
-var socketHandler = new SocketsHttpHandler { PooledConnectionLifetime = TimeSpan.FromMinutes(15) };
-var resilienceHandler = new ResilienceHandler(retryPolicy) { InnerHandler = socketHandler };
-var httpClient = new HttpClient(resilienceHandler);
-#else
-
-var retryPolicy = HttpPolicyExtensions
-    .HandleTransientHttpError()
-    .WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)), QuickLog);
-
-var socketHandler = new SocketsHttpHandler { PooledConnectionLifetime = TimeSpan.FromMinutes(15) };
-var pollyHandler = new PolicyHttpMessageHandler(retryPolicy) { InnerHandler = socketHandler };
-var httpClient = new HttpClient(pollyHandler);
-#endif
-
-httpClient.BaseAddress = new Uri($"https://localhost:7096");
-
+var httpClient = ResilientClientFactory.Make(new Uri($"https://localhost:7096"), ResilientClientFactory.QuickLog);
 var example = new ExampleClient(httpClient);
 
 var i = await example.GetSecretAsync("example", "example", "fake"); //env/app/key
@@ -37,7 +10,6 @@ if (i is not null) { Console.WriteLine($"Found {i.Key}!"); }
 Console.ReadKey();
 
 // Helpers
-void QuickLog(DelegateResult<HttpResponseMessage> result, TimeSpan span) => Console.WriteLine("Retry");
 
 internal sealed class ExampleClient(HttpClient client)
 {
