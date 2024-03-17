73374f36abbe2724d280464baa5d0f0227ead63c
Alex Salisbury <alex@alexsalisbury.com>
2024-03-17 14:59:53 -0500

    A quick, lazy bot

---
+++ src/Quick/QuickBot/BotOptions.cs
@@ -0,0 +1,10 @@
+﻿using QuickTools.Core;
+
+public interface IBotOptions : IResilientConfigClientOptions { }
+
+public class BotOptions : IBotOptions
+{
+    public string ConfigServiceUrl { get; set; }
+    public string App { get; set; }
+    public string Env { get; set; }
+}
