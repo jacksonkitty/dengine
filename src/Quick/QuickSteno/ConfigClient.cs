e73a3282261e20e76e4634d6fb7783eabb3b0fd0
Alex Salisbury <alex@alexsalisbury.com>
2024-03-17 08:08:33 -0500

    Steno refactor: Core, resilient ConfigClient<T>, StenoOptions,

--- src/Quick/QuickSteno/ConfigClient.cs
+++ src/Quick/QuickSteno/ConfigClient.cs
@@ -1,49 +1,31 @@
 ﻿namespace QuickSteno;
 
 using System.Net.Http.Json;
-using Microsoft.Extensions.Http;
-using Polly;
-using Polly.Extensions.Http;
+using QuickTools.Core;
 
 public record ConfigValue(string Env, string App, string Key, string Value, DateTimeOffset? ExpiresOn = null, bool IsValid = false, bool IsSecure = false);
 
-public sealed class ConfigClient(HttpClient client)
+public abstract class ConfigClient<T>
 {
-    public static HttpClient MakeClient(Uri target)
+    private IResilientConfigClientOptions options;
+    private HttpClient client;
+
+    public ConfigClient(IResilientConfigClientOptions options)
     {
-        //https://learn.microsoft.com/en-us/dotnet/fundamentals/networking/http/httpclient-guidelines
-#if RESILIENCE_HANDLER_IS_NO_LONGER_EVAL
-var retryPipeline = new ResiliencePipelineBuilder<HttpResponseMessage>()
-    .AddRetry(new HttpRetryStrategyOptions
-    {
-        BackoffType = DelayBackoffType.Exponential,
-        MaxRetryAttempts = 3
-    })
-    .Build();
-
-var socketHandler = new SocketsHttpHandler { PooledConnectionLifetime = TimeSpan.FromMinutes(15) };
-var resilienceHandler = new ResilienceHandler(retryPolicy) { InnerHandler = socketHandler };
-var httpClient = new HttpClient(resilienceHandler);
-#else
-
-        var retryPolicy = HttpPolicyExtensions.HandleTransientHttpError()
-            .WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)), QuickLog);
-
-        var socketHandler = new SocketsHttpHandler { PooledConnectionLifetime = TimeSpan.FromMinutes(15) };
-        var pollyHandler = new PolicyHttpMessageHandler(retryPolicy) { InnerHandler = socketHandler };
-        var httpClient = new HttpClient(pollyHandler);
-#endif
-
-        httpClient.BaseAddress = target;
-        return httpClient;
+        this.options = options;
+        this.client = ResilientClientFactory.Make(options);
     }
 
-    public async Task<ConfigValue?> GetSecretAsync(string env, string app, string key)
+    public async Task<T?> GetSecretAsync(string key)
     {
-        var path= $"/secrets/{env}/{app}/{key}";
-        var test = await client.GetAsync(path);
-        return await client.GetFromJsonAsync<ConfigValue>(path, CancellationToken.None);
+        var path = $"/secrets/{options.Env}/{options.App}/{key}";
+       // var test = await client.GetAsync(path);
+        return await client.GetFromJsonAsync<T>(path, CancellationToken.None);
     }
+}
 
-    static void QuickLog(DelegateResult<HttpResponseMessage> result, TimeSpan span) => Console.WriteLine("Retry");
+
+public sealed class ExampleClient: ConfigClient<ConfigValue>
+{
+    public ExampleClient(IResilientConfigClientOptions options):base(options) { }
 }
---
+++ src/Quick/QuickSteno/StenoOptions.cs
@@ -0,0 +1,24 @@
+﻿namespace QuickSteno;
+
+using QuickTools.Core;
+
+public interface INoteManagerOptions
+{
+    string NoteRootFolder { get; set; }
+}
+
+public interface IResilientConfigClientOptions : IResilientClientOptions
+{
+    string App { get; set; }
+    string Env { get; set; }
+}
+
+public interface IStenoOptions : INoteManagerOptions, IResilientConfigClientOptions { }
+
+public class StenoOptions : IStenoOptions
+{
+    public string ConfigServiceUrl { get; set; }
+    public string NoteRootFolder { get; set; }
+    public string App { get; set; }
+    public string Env { get; set; }
+}
